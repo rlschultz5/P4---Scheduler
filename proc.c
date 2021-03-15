9d5a03a536846a14554b28ac16137dfdf7b7d1a9
Rob Schultz <rlschultz5@wisc.edu>
2021-03-14 20:49:12 -0500

    no message

--- proc.c
+++ proc.c
@@ -140,7 +140,7 @@ userinit(void)
 
   p = allocproc();
   
-//  initproc = p; // P4: Set first head as initproc
+  initproc = p;
   if((p->pgdir = setupkvm()) == 0)
     panic("userinit: out of memory?");
   inituvm(p->pgdir, _binary_initcode_start, (int)_binary_initcode_size);
