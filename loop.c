8ee23ce19d7c852adce7c50296f46c14a307dea8
Rob Schultz <rlschultz5@wisc.edu>
2021-03-16 22:37:41 -0500

    no message

--- loop.c
+++ loop.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
         printf(0,"Invalid parameters. Pass in number of sleeps only");
         return -1;
     }
-    int sleepT = argv[1];
+    int sleepT = atoi(argv[1]);
     if(sleepT < 1) {
         printf(0,"Number of sleeps must be greater than 0.");
         return -1;
