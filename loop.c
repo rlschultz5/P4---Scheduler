a901d64f90ff2a2a86d2721177bdf13b979bb38f
Rob Schultz <rlschultz5@wisc.edu>
2021-03-16 22:34:24 -0500

    no message

--- loop.c
+++ loop.c
@@ -7,22 +7,24 @@
 int main(int argc, char *argv[]) {
     if(argc != 2) {
         printf(0,"Invalid parameters. Pass in number of sleeps only");
+        return -1;
     }
-
-    sleep(atoi(argv[1]));
+    int sleepT = argv[1];
+    if(sleepT < 1) {
+        printf(0,"Number of sleeps must be greater than 0.");
+        return -1;
+    }
+    sleep(sleepT);
     // First, sleep for sleepT ticks;
 //    for(int i = 0; i < argv[1]); i++) {
 //        sleep();
 //    }
-
 
     int i = 0, j = 0;
     while (i < 800000000) {
         j += i * j + 1;
         i++;
     }
-
-
 }
 
 
@@ -31,10 +33,10 @@ int main(int argc, char *argv[]) {
 
 //int main(void)
 //{
-////  printf(0,"%d\n", setslice(1,7));
+//  printf(0,"%d\n", setslice(1,7));
 //  printf(0,"%d\n", getslice(1));
-////  printf(0,"%d\n", getslice(1));
-////  printf(0,"%d\n", getslice(2));
+//  printf(0,"%d\n", getslice(1));
+//  printf(0,"%d\n", getslice(2));
 //  int pid = fork2(3);
 //
 //  if (pid != -1) {
@@ -57,7 +59,7 @@ int main(int argc, char *argv[]) {
 //            free(trial);
 //
 //            char *args[2] = {"ls", 0};
-////            printf(0,"%d\n", setslice(4,90));
+//            printf(0,"%d\n", setslice(4,90));
 //            printf(0,"%d\n", getslice(3));
 //            exec(args[0], args);
 //
@@ -79,28 +81,28 @@ int main(int argc, char *argv[]) {
 //    }
 //
 //
-////  printf(0,"DONE FORKING\n");
+//  printf(0,"DONE FORKING\n");
 //
-////  struct pstat *trial = malloc (sizeof (struct pstat));
-//////   printf(0,"DID it WORK?? %d  OR NAHHH\n",getpinfo(trial));
-////    getpinfo(trial);
-////    for(int i = 0; i < 1; i++) {
-////     printf(0,"inuse: %d  pid: %d  timeslice: %d  compticks: %d"
-////     "  schedticks: %d  sleepticks: %d  switches: %d\n",
-////     trial->inuse[i],trial->pid[i],trial->timeslice[i],
-////     trial->compticks[i],trial->schedticks[i],trial->sleepticks[i],
-////     trial->switches[i]);
-////    }
-////    free(trial);
+//  struct pstat *trial = malloc (sizeof (struct pstat));
+//   printf(0,"DID it WORK?? %d  OR NAHHH\n",getpinfo(trial));
+//    getpinfo(trial);
+//    for(int i = 0; i < 1; i++) {
+//     printf(0,"inuse: %d  pid: %d  timeslice: %d  compticks: %d"
+//     "  schedticks: %d  sleepticks: %d  switches: %d\n",
+//     trial->inuse[i],trial->pid[i],trial->timeslice[i],
+//     trial->compticks[i],trial->schedticks[i],trial->sleepticks[i],
+//     trial->switches[i]);
+//    }
+//    free(trial);
 //
 //        // Then, call a loop like this which loops on a huge workload (don't
 //        // try to code and run any real programs like this! It is just for
 //        // testing purpose of this project):
-////        int i = 0, j = 0;
-////        while (i < 800000000) {
-////            j += i * j + 1;
-////            i++;
-////        }
-////    }
+//        int i = 0, j = 0;
+//        while (i < 800000000) {
+//            j += i * j + 1;
+//            i++;
+//        }
+//    }
 //  exit();
 //}
